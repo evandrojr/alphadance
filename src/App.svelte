5951cf140b605523909603ac7af158f87da40a40
Evandro Junior <evandrojr@gmail.com>
2019-10-23 07:30:45 -0300

    antes de centralizar

--- src/App.svelte
+++ src/App.svelte
@@ -1,114 +1,139 @@
 <script>
 	export let atividades;
-	
+
 	function tranformarParaAtividadesPorHora() {
-		let trans=[]; 
+		let trans = [];
 		let transAtividadesPorDia;
 		let transAtividadesAgrupadasPorHora;
-		const nomesAtividades = {f: 'FitDance', z: 'Zumba' };	
-		
+		const nomesAtividades = { f: 'FitDance', z: 'Zumba' };
+
 		atividades.forEach(atividadesPorDia => {
-			transAtividadesPorDia = {};	
+			transAtividadesPorDia = {};
 			transAtividadesPorDia.diaSemana = atividadesPorDia.diaSemana;
-			transAtividadesPorDia.horarios = [];	
-			for(let h=6; h<=20; ++h){
+			transAtividadesPorDia.horarios = [];
+			for (let h = 6; h <= 20; ++h) {
 				atividadesPorDia.locais.forEach(local => {
-			//  console.log('local', local);
-			//  console.log('local.aulas', local.aulas);
-				Object.entries(local.aulas).forEach(aulasHorarios => {
-					console.log('aulasHorarios', aulasHorarios);
-					aulasHorarios[1].forEach(horario => {
-						console.log('horario', horario);
-						if(horario==h){
-							transAtividadesPorDia.horarios.push({hora: horario, atividade: nomesAtividades[aulasHorarios[0]], local: local.nome }); 
-						}
+					//  console.log('local', local);
+					//  console.log('local.aulas', local.aulas);
+					Object.entries(local.aulas).forEach(aulasHorarios => {
+						console.log('aulasHorarios', aulasHorarios);
+						aulasHorarios[1].forEach(horario => {
+							console.log('horario', horario);
+							if (horario == h) {
+								transAtividadesPorDia.horarios.push({ hora: horario, atividade: nomesAtividades[aulasHorarios[0]], local: local.nome });
+							}
+						});
 					});
 				});
-			 });
-		 }
+			}
 
-		transAtividadesAgrupadasPorHora = transAtividadesPorDia.horarios.reduce(function (r, a) {
+			transAtividadesAgrupadasPorHora = transAtividadesPorDia.horarios.reduce(function (r, a) {
 				r[a.hora] = r[a.hora] || [];
 				r[a.hora].push(a);
 				return r;
-		}, Object.create(null));
-		transAtividadesPorDia.horarios= transAtividadesAgrupadasPorHora;
+			}, Object.create(null));
+			transAtividadesPorDia.horarios = transAtividadesAgrupadasPorHora;
 
-		 trans.push(transAtividadesPorDia);	
-			
+			trans.push(transAtividadesPorDia);
+
 		});
-		
-		console.log('trans',trans);
-		
+
+		// console.log('trans', trans);
+
 		return trans;
-  	}
-	
+	}
+
 </script>
 
 <style>
-	h1 {
-		color: yellow;
-		text-shadow: 1px 1px #000000;
+	[atividades-por-horario] {
+		border-color: #200939;
+		border-style: solid;
+		display: flex;
 	}
 
-    [atividades-por-horario] {
-      border-color: black;
-      border-style: solid;
-      display: flex;
-	  margin: .2rem;
-	  padding: .2rem;
-
-    }
-
-    [horario] {
-	  width: 2.5em;	
-      padding: 1rem;
-	  text-align: center;
-	}
-	
-	[topo] {
-		padding: .5em;
+	[horario] {
+		min-width: 70px;
+		padding: 1rem;
 		text-align: center;
-		border-bottom: solid 1px;
+		color: #200939;
 	}
 
-	[preto]{
+	[topo] {
+		/* padding: .1em; */
+		/* text-align: center; */
+		display: flex;
+		/* flex-direction: column; */
+		/* padding: 1rem; */
+		/* border-bottom: solid 1px; */
+	}
+
+	/* [preto]{
 		color: #000000;
-	}
+	} */
 
-	[fundo-amarelo]{
+	[fundo-amarelo] {
 		background-color: #EEC82F;
-		
+
 	}
 
-	[local-e-atividade]{
+	[local-e-atividade] {
 		padding-left: .5em;
+		width: 400px;
+		text-align: left;
+	}
+
+	a {
+		color: #EEC82F;
+	}
+
+	[centralizador] {
+		width: 30%;
+		/* text-align: center; */
+		/* display: flex; */
+
+	}
+
+	[espacador] {
+		width: 100%;
+	}
+
+	[programacao] {
+		display: flex;
 
 	}
 </style>
 
-<div topo fundo-amarelo>
-<h1 preto>alphadance</h1>
+
+<div topo>
+	<div espacador></div>
+	<div>
+		<img src="images/alphadance.png" alt="alphadance">
+		<!-- <br> -->
+		<span>Horários não oficiais das aulas de dança nas unidades da Rede Alpha. Achou algum erro ou tem alguma
+			sugestão? Contato: <a
+				href="https://api.whatsapp.com/send?phone=5571982864766&text=Ol%C3%A1%20tenho%20uma%20sugest%C3%A3o%20sobre%20o%20alphadance.">Whatsapp:
+				(71)98286-4766</a>
+		</span>
+
+
+	</div>
+	<div espacador></div>
 </div>
 
-<div>
+
+
 
 {#each tranformarParaAtividadesPorHora() as dia}
-    <h2>{dia.diaSemana}</h2>
-	{#each Object.keys(dia.horarios) as hora}
-		<div atividades-por-horario>
-		<div horario fundo-amarelo>{hora}:10</div>
-		<div>
-		{#each Object.values(dia.horarios[hora]) as aula}
-			<div local-e-atividade>{aula.local}: {aula.atividade}</div>
-		{/each}
-		</div>
-		</div>
+				<h2>{dia.diaSemana}</h2>
+				{#each Object.keys(dia.horarios) as hora}
+					<div atividades-por-horario>
+						<div horario fundo-amarelo>{hora}:10</div>
+							<div>
+								{#each Object.values(dia.horarios[hora]) as aula}
+									<div local-e-atividade>{aula.local}: {aula.atividade}</div>
+								{/each}
+							</div>
+					</div>
+				{/each}
 	{/each}
-{/each}
-
- 
-
-
-
-  </div>
